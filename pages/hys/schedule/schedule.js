f14d3df6711149c10a0e58c5a2493dda3b3bed53
baozi <baozi>
2018-07-10 20:59:09 +0800

    ji

---
+++ pages/hys/schedule/schedule.js
@@ -0,0 +1,97 @@
+var app = getApp()
+var util = require('../../../utils/util.js')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    params: {
+      randomIds: '14281909272509',
+      dateTime:'',
+    },
+    dataList:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    util.requestLoading('/rest/api/findScheduleByConference', this.data.params, '正在加载',
+      function (res) {
+        if (res.code == 200) {
+          that.data.dataList = res.dateList;
+          console.info(that.data.dataList);
+          //跳转不同页面
+          
+        } else if (res.code == 400) {
+          //弹窗提醒异常
+          const dataInfo = { content: res.message };
+          util.showMessage(dataInfo);
+
+        } else if (res.code == 410) {
+          //跳页面提醒异常
+          wx.redirectTo({
+            url: '../../common/error/error?message=' + res.message
+          })
+        } else if (res.code == 420) {
+          //登陆超时
+          util.login({ url: '../index' });
+        }
+      }, function () {
+        wx.showToast({
+          title: '提交失败',
+        })
+      })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
